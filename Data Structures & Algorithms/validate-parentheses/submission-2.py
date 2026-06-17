6f3fd6113d3049661df4e302d9336704f9fa67a7
Vignesh M <132240325+0x0906@users.noreply.github.com>
2026-06-17 21:04:42 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        dic = { ")" : "(", "]" : "[", "}" : "{" }
+        
+        for d in s:
+            if d in "({[":
+                stack.append(d)
+            else:
+                if not stack:
+                    return False
+                    
+                if stack.pop() != dic[d]:
+                    return False
+
+        return len(stack) == 0
+
+# ([{ -> push it onto the stack.
+# }]) -> check whether it matches the most recent opening bracket.
+
+# stack = []
+
+# '{' -> push
+# stack = ['{']
+
+# '[' -> push
+# stack = ['{', '[']
+
+# ']' -> matches '['
+# stack = ['{']
+
+# '}' -> matches '{'
+# stack = []
